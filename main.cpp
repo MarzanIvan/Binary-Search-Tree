c9179d126b1138fba5ec6db97faa0af3f4fe6bfd
Marzan Ivan <boss.marzan@bk.ru>
2023-06-05 23:34:19 +0300

    create function (main.cpp::Heapify) to reorder the binary heap

--- main.cpp
+++ main.cpp
@@ -1,6 +1,36 @@
 ﻿#include <iostream>
 
-int main() {
+using namespace std;
 
-	std::cout << "Hello World!\n";
+int* Heapify(int* array, int i, int size) {
+	int left = (i << 1) + 1;
+	int right = (i << 1) + 2;
+	int max = i;
+	if (left <= size && array[left] > array[max]) max = left;
+	if (right <= size && array[right] > array[max]) max = right;
+	if (max != i) {
+		auto temp = array[max];
+		array[max] = array[i];
+		array[i] = temp;
+		Heapify(array, max, size);
+	}
+	return array;
+}
+
+void showarray(char* letter) {
+	while (*(letter++)) {
+		std::cout << *letter;
+	}
+}
+
+void showarray(int* array, int size) {
+	for (size_t i = 0; i < size; i++) {
+		cout << *(array++) << ' ';
+	}
+}
+
+int main() {
+	int array[10] = { 16,17,9,10,5,6,8,1,2,4 };
+	Heapify(array,0,10);
+	showarray(array, 10);
 }
