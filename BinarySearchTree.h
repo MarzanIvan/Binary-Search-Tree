e77465453777e8e1a7b6619f6498f3c9b9956ff9
Marzan Ivan <boss.marzan@bk.ru>
2023-06-05 23:42:17 +0300

    add implemented function Heapify to template structure BinarySearchTree

--- BinarySearchTree.h
+++ BinarySearchTree.h
@@ -15,5 +15,27 @@ struct BinarySearchTree {
 			delete[] array;
 		}
 	}
+
+	void Heapify(int index, size_t size);
+	
 };
+
+template <class Ttype>
+void BinarySearchTree<Ttype>::Heapify(int index, size_t size) {
+	int LeftNodeIndex = (i << 1) + 1;
+	int RightNodeIndex = (i << 1) + 2;
+	int MaxNodeIndex = index;
+	if (LeftNodeIndex <= size && array[LeftNodeIndex] > array[MaxNodeIndex]) {
+		MaxNodeIndex = LeftNodeIndex;
+	}
+	if (RightNodeIndex <= size && array[RightNodeIndex] > array[MaxNodeIndex]) {
+		MaxNodeIndex = RightNodeIndex;
+	}
+	if (MaxNodeIndex != index) {
+		auto temp = array[MaxNodeIndex];
+		array[MaxNodeIndex] = array[index];
+		array[index] = temp;
+		Heapify(MaxNodeIndex, size);
+	}
+}
 
